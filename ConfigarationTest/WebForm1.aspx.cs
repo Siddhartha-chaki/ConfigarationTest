e8e3722a440284f132d42ae85b1c1802bd6414ab
Administrator <Administrator@HCL-PC23>
2017-09-06 09:54:44 +0530

    Add project files.

---
+++ ConfigarationTest/WebForm1.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+
+namespace ConfigarationTest
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            String country,PrimeMinisrter,President;
+            country = ConfigurationManager.AppSettings["Country"];
+            PrimeMinisrter = ConfigurationManager.AppSettings["PrimeMinister"];
+            President = ConfigurationManager.AppSettings["President"];
+            Response.Write("Country: "+country+" <br/>PrimeMinister: "+PrimeMinisrter+"</br>President:"+President);
+        }
+    }
+}
